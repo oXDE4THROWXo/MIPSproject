f9df23badb1e5f01b4360598405993612ec6e454
oXDE4THROWXo <chrisredwine45@yahoo.com>
2018-10-19 13:34:34 -0400

    Update main.asm

--- main.asm
+++ main.asm
@@ -3,3 +3,8 @@
     li $v0, 11        # Print chars
     la $a0, 64        # @ (64)
     syscall
+    sub $v0, 10       # Print integers
+    sub $a0, 64       # 0
+    syscall
+    addi $a0, 2       # 2
+    syscall        
